f1e16085efa2a31ce7d7e0dc8c0afe5122ddbeab
Bartosz Bukowiec <bartosz.bukowiec@synerise.com>
2021-09-13 18:38:06 +0200

    refactor get_face_detector

--- face_detector.py
+++ face_detector.py
@@ -4,19 +4,10 @@ import cv2
 import numpy as np
 
 
-def get_face_detector(modelFile=None, configFile=None, quantized=False):
-    if quantized:
-        if modelFile == None:
-            modelFile = "models/opencv_face_detector_uint8.pb"
-        if configFile == None:
-            configFile = "models/opencv_face_detector.pbtxt"
-        model = cv2.dnn.readNetFromTensorflow(modelFile, configFile)
-    else:
-        if modelFile == None:
-            modelFile = "models/res10_300x300_ssd_iter_140000.caffemodel"
-        if configFile == None:
-            configFile = "models/deploy.prototxt"
-        model = cv2.dnn.readNetFromCaffe(configFile, modelFile)
+def get_face_detector():
+    modelFile = "models/res10_300x300_ssd_iter_140000.caffemodel"
+    configFile = "models/deploy.prototxt"
+    model = cv2.dnn.readNetFromCaffe(configFile, modelFile)
     return model
 
 
